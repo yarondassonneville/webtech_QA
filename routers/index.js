1d4768a15e1d8ed34aec3490896311ca6e08c888
Yaron Dassonneville <Yaron@MacBook-Pro-van-Yaron.local>
2016-04-03 14:08:23 +0200

    Made a login form

--- routers/index.js
+++ routers/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Hey', discussion: 'Hello there!'});
+    res.render('index', { title: 'Hey', discussion: 'Log in'});
     // Deze 2 regels kunnen niet tegelijkertijd afgaan 
     // res.json({ message: 'hooray! welcome to our api!' });
 

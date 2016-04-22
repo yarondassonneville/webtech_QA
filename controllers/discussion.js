018d5ecb9462d0b9c9475ecb61069a7b9f6730db
Taylorvincent <vincent.sam.taylor@gmail.com>
2016-04-22 19:38:30 +0200

    User in session, can GET a single discussion, organised jade file structure
    ook de code beetje gekuist (indenting en duidelijkere console.logs)

--- controllers/discussion.js
+++ controllers/discussion.js
@@ -1,13 +1,15 @@
 var Discussion = require('./../models/discussion');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+
 
 function getAll(req, res){
     var allDiscussions;
     Discussion.find(function(err, discussions){
         if (err) return console.error(err);
         allDiscussions = discussions;
-        return res.render('discussion', {
+        return res.render('discussion/all', {
             date: new Date().toDateString(),
             allDiscussions: allDiscussions
           });
@@ -17,8 +19,8 @@ function getAll(req, res){
 module.exports.getAll = getAll;
 
 function create(req, res){
-
-    var discussion = new Discussion({ topic: req.body.newDiscussion, user: req.body.userNameField });
+    var sess = session;
+    var discussion = new Discussion({ topic: req.body.newDiscussion, userID: sess.userID, userName: sess.userName });
     discussion.save(function (err, discussion) {
     if (err) return console.error(err);
     console.log('succes! new discussion topic ' + discussion.topic);
@@ -28,3 +30,15 @@ function create(req, res){
 };
 
 module.exports.create = create;
+
+function getDiscussion(req, res, pTopic){
+    Discussion.findOne({'topic': pTopic}, 'user topic _id', function (err, discussion) {
+        if (err) return handleError(err);
+        console.log('user %s maakte de topic -> %s aan. ID = %s', discussion.user, discussion.topic, discussion._id);
+        // Space Ghost is a talk show host.)
+    });
+};
+
+
+
+module.exports.getDiscussion = getDiscussion;
--- models/discussion.js
+++ models/discussion.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 var discussionSchema = mongoose.Schema({
-    user: String,
+    userID: String,
+    userName: String,
     topic: String
 });
 

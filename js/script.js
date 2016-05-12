affcdf225cc88b2f6a4699f443a42a5352724107
Taylorvincent <vincent.sam.taylor@gmail.com>
2016-05-12 04:17:46 +0200

    start tut

--- js/script.js
+++ js/script.js
@@ -6,13 +6,19 @@ $(document).ready(function(){
             console.log("Geolocation is not supported by this browser.");
         }
     }
-    
+
     function showPosition(position) {
         var lat = position.coords.latitude;
         var lng = position.coords.longitude;
         $("#locLat").val(position.coords.latitude);
         $("#locLng").val(position.coords.longitude);
     }
-    
+
     getLocation();
+
+    var socket = io.connect('http://localhost:3000');
+    socket.on('news', function (data) {
+      console.log(data);
+      socket.emit('my other event', { my: 'data' });
+    });
 });
